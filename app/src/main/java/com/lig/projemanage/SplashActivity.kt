49c56ff91bd6bbc51b5b8a2ebd36259eae68ac2f
guopeng li <guopeng.li@technicolor.com>
2020-10-19 11:23:54 +0200

    post delay to pass launcher to intro activity

--- app/src/main/java/com/lig/projemanage/SplashActivity.kt
+++ app/src/main/java/com/lig/projemanage/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.lig.projemanage
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.Window
 import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_splash.*
@@ -19,5 +21,12 @@ class SplashActivity : AppCompatActivity() {
         )
         val typeFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
         tv_app_name.typeface = typeFace
+
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish() // close activity after very important
+        }, 2500
+        )
+
     }
 }
