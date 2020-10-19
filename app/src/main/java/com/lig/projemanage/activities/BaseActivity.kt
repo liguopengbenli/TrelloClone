1c8fcd8564397639c30870e4efc1ce56c81f72fc
guopeng li <guopeng.li@technicolor.com>
2020-10-19 15:29:41 +0200

    Create a progress dialog

--- app/src/main/java/com/lig/projemanage/activities/BaseActivity.kt
+++ app/src/main/java/com/lig/projemanage/activities/BaseActivity.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lig.projemanage.R
+import kotlinx.android.synthetic.main.dialog_progress.*
 
 open class BaseActivity : AppCompatActivity() {
 
@@ -15,6 +16,19 @@ open class BaseActivity : AppCompatActivity() {
         setContentView(R.layout.activity_base)
     }
 
+    fun showProgressDialog(text: String){
+        mProgressDialog = Dialog(this)
 
+        mProgressDialog.setContentView(R.layout.dialog_progress)
+        mProgressDialog.tv_progress_text.text = text
+
+        mProgressDialog.show()
+    }
+
+    fun hideProgressDialog(){
+        mProgressDialog.dismiss()
+    }
+
+    
 
 }
