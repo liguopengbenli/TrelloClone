af7d9aeea312d6bccbc56213b064b92cc0a694ee
guopeng li <guopeng.li@technicolor.com>
2020-10-19 16:30:09 +0200

    Sign UP activity - get valid text from user

--- app/src/main/java/com/lig/projemanage/activities/BaseActivity.kt
+++ app/src/main/java/com/lig/projemanage/activities/BaseActivity.kt
@@ -3,6 +3,11 @@ package com.lig.projemanage.activities
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import com.lig.projemanage.R
 import kotlinx.android.synthetic.main.dialog_progress.*
 
@@ -29,6 +34,28 @@ open class BaseActivity : AppCompatActivity() {
         mProgressDialog.dismiss()
     }
 
-    
+    fun getCurrentUserID(): String{
+        return FirebaseAuth.getInstance().currentUser!!.uid
+    }
+
+    // this function can make sure user doesn't close activity accidentally
+    fun doubleBackToExit(){
+        if(doubleBackToExitPressedOnce){
+            super.onBackPressed()
+            return
+        }
+        this.doubleBackToExitPressedOnce = true // first time true second time go back
+
+        Toast.makeText(this, resources.getString(R.string.please_click_back_again_to_exit), Toast.LENGTH_SHORT)
+        Handler().postDelayed({doubleBackToExitPressedOnce = false}, 2000)
+    }
+
+    fun showErrorSnackBar(message: String){
+        val snackBar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
+        snackBarView.setBackgroundColor(ContextCompat.getColor(this, R.color.snackbar_error_color))
+        snackBar.show()
+    }
+
 
 }
--- app/src/main/java/com/lig/projemanage/activities/SignUpActivity.kt
+++ app/src/main/java/com/lig/projemanage/activities/SignUpActivity.kt
@@ -2,7 +2,10 @@ package com.lig.projemanage.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.WindowManager
+import android.widget.Toast
 import com.lig.projemanage.R
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
@@ -16,6 +19,10 @@ class SignUpActivity : BaseActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
         setupActionBar()
+
+        btn_sign_up.setOnClickListener {
+            registerUser()
+        }
     }
 
 
@@ -26,5 +33,33 @@ class SignUpActivity : BaseActivity() {
         toolbar_sign_up_activity.setNavigationOnClickListener { onBackPressed() }
     }
 
+    private fun registerUser(){
+        val name: String = et_name.text.toString().trim{ it <= ' '} // trim to remove the char in the string
+        val email: String = et_email.text.toString().trim{ it <= ' '}
+        val password: String = et_password.text.toString().trim{ it <= ' '}
+
+        if(validateForm(name, email, password)){
+            Log.i("signup", "success")
+        }else{
+            Log.i("signup", "failed")
+        }
+    }
+
+    private fun validateForm(name: String, email: String, password: String): Boolean{
+        return when {
+            TextUtils.isEmpty(name)->{
+                showErrorSnackBar("Please enter a name")
+                false
+            }
+            TextUtils.isEmpty(email)->{
+                showErrorSnackBar("Please enter a email")
+                false
+            }
+            TextUtils.isEmpty(password)->{
+                showErrorSnackBar("Please enter a password")
+                false
+            }else ->{true}
+        }
+    }
 
 }
