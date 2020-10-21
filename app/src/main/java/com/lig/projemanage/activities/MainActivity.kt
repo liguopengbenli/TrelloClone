365578f47693b065721a4e588abce1890b256546
guopeng li <guopeng.li@technicolor.com>
2020-10-21 10:29:33 +0200

    Layout menu group-items + nav header

--- app/src/main/java/com/lig/projemanage/activities/MainActivity.kt
+++ app/src/main/java/com/lig/projemanage/activities/MainActivity.kt
@@ -2,11 +2,17 @@ package com.lig.projemanage.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.lig.projemanage.R
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
     }
 }
