a88361f468da700edc29efa09fd1143a92685db6
guopeng li <guopeng.li@technicolor.com>
2020-10-22 15:12:29 +0200

    recycle view adapter

---
+++ app/src/main/java/com/lig/projemanage/adapters/BoardItemsAdapter.kt
@@ -0,0 +1,49 @@
+package com.lig.projemanage.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.lig.projemanage.R
+import com.lig.projemanage.models.Board
+import kotlinx.android.synthetic.main.item_board.view.*
+import kotlinx.android.synthetic.main.nav_header_main.*
+
+open class BoardItemsAdapter(private val context: Context, private var list: ArrayList<Board>):
+            RecyclerView.Adapter<RecyclerView.ViewHolder>()
+
+
+{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+       return MyViewHolder(LayoutInflater.from(context)
+           .inflate(R.layout.item_board, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+        if(holder is MyViewHolder){
+            Glide
+                .with(context)
+                .load(model.image)
+                .centerCrop()
+                .placeholder(R.drawable.ic_user_place_holder)
+                .into(holder.itemView.iv_board_image_recycle)
+
+            holder.itemView.tv_name.text = model.name
+            holder.itemView.tv_created_by.text = "Created by: ${model.createdBy}"
+            
+        }
+    }
+
+
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    private class MyViewHolder(view: View): RecyclerView.ViewHolder(view)
+
+
+}
