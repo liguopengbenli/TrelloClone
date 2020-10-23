e65fb1233df6e49bbbd4d0de596d7ba20849b305
guopeng li <guopeng.li@technicolor.com>
2020-10-23 17:08:55 +0200

    implemente adapter

--- app/src/main/java/com/lig/projemanage/activities/TaskListActivity.kt
+++ app/src/main/java/com/lig/projemanage/activities/TaskListActivity.kt
@@ -3,9 +3,12 @@ package com.lig.projemanage.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.lig.projemanage.R
+import com.lig.projemanage.adapters.TaskListItemsAdapter
 import com.lig.projemanage.firebase.FireStoreClass
 import com.lig.projemanage.models.Board
+import com.lig.projemanage.models.Task
 import com.lig.projemanage.utils.Constants
 import kotlinx.android.synthetic.main.activity_my_profile.*
 import kotlinx.android.synthetic.main.activity_task_list.*
@@ -41,6 +44,14 @@ class TaskListActivity : BaseActivity() {
     fun boardDetails(board: Board){
         hideProgressDialog()
         setupActionBar(board.name)
+
+        val addTaskList = Task(resources.getString(R.string.add_list))
+        board.taskList.add(addTaskList)
+        rv_task_list.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rv_task_list.setHasFixedSize(true)
+
+        val adapter = TaskListItemsAdapter(this, board.taskList)
+        rv_task_list.adapter = adapter
     }
 
 
