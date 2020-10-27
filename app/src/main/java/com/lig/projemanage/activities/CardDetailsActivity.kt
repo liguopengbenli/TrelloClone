7276b68cfba9791017d0eda15ca895d0da998bb0
guopeng li <guopeng.li@technicolor.com>
2020-10-27 10:50:33 +0100

    Create Cards details activity with its UI cards details

---
+++ app/src/main/java/com/lig/projemanage/activities/CardDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.lig.projemanage.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lig.projemanage.R
+
+class CardDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_details)
+    }
+}
